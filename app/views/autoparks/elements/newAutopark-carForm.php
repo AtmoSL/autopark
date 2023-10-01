6e3f3d03854138c62905cfe1ada87c63802fdfb0
evdokilya <devdokimov.ilya@yandex.ru>
2023-10-02 02:10:23 +0300

    Добавил возможность удаления машин на странице создания автопарка

--- app/views/autoparks/elements/newAutopark-carForm.php
+++ app/views/autoparks/elements/newAutopark-carForm.php
@@ -1,22 +1,7 @@
-
 <div class="cars" id="all-cars">
-<?php if (empty($_SESSION["autoparkCreate-form"]["cars"])) { ?>
-        <div class="car__row d-flex justify-content-between">
-            <label for="number" class="form-label text-center">Номер машины
-                <input type="text"
-                       name="cars[car0][number]"
-                       id="number"
-                       class="form-control"></label>
-
-            <label for="driver_name" class="form-label text-center">Имя водителя
-                <input type="text"
-                       name="cars[car0][driver_name]"
-                       id="driver_name"
-                       class="form-control"> </label>
-        </div>
-<?php } else {
+<?php if (!empty($_SESSION["autoparkCreate-form"]["cars"])) {
     foreach ($_SESSION["autoparkCreate-form"]["cars"] as $key => $value) { ?>
-            <div class="car__row d-flex justify-content-between">
+            <div class="car__row d-flex justify-content-between" id="car-row-<?= $key ?>">
                 <label for="number" class="form-label text-center">Номер машины
                     <input type="text"
                            name="cars[<?= $key ?>][number]"
@@ -31,6 +16,7 @@
                            id="driver_name"
                            class="form-control"
                            value="<?= $value["driver_name"] ?>"> </label>
+                <div class="delete_car" id="delete-<?= $key ?>">Х</div>
             </div>
             <?php if (isset($_SESSION["autoParkCars-messages"][$key])) { ?>
                 <div class="form__errors">
@@ -49,4 +35,4 @@
 <?php } ?>
 
 </div>
-<div class="btn btn-warning w-100" id="add-car">Добавить ещё одну машину</div>
+<div class="btn btn-warning w-100" id="add-car">Добавить машину</div>
--- resources/main.js
+++ resources/main.js
@@ -1,41 +1,55 @@
 const addCarBtn = document.getElementById("add-car");
 const allCars = document.getElementById("all-cars");
-let carCounter = document.querySelectorAll('.car__row').length;
 const deleteAutoparkBtn = document.getElementById("deleteAutopark");
 
-
 //Предупреждение при удалении автопарка
-deleteAutoparkBtn.addEventListener("click",(event)=>{
-    const isDelete = confirm("Автопарк будет удалён без возможности восстановления. Машины останутся в базе.");
+if (deleteAutoparkBtn) {
+    deleteAutoparkBtn.addEventListener("click", (event) => {
+        const isDelete = confirm("Автопарк будет удалён без возможности восстановления. Машины останутся в базе.");
 
-    if(isDelete){
-        window.location.href = '/autopark/delete?id=' + deleteAutoparkBtn.dataset.autoparkId;
-    }
+        if (isDelete) {
+            window.location.href = '/autopark/delete?id=' + deleteAutoparkBtn.dataset.autoparkId;
+        }
 
-});
+    });
+}
 
 //Добавление поля для машины на странице создания автопарка
-addCarBtn.addEventListener("click", (event) => {
-    const carForm = createCarForm(carCounter);
-    carCounter++;
-    allCars.append(carForm);
-});
+if (addCarBtn) {
+    addCarBtn.addEventListener("click", (event) => {
+        const carId = Math.random();
+        const carForm = createCarForm(carId);
+        allCars.append(carForm);
+
+        const deleteBtn = document.getElementById("delete-" + carId );
+
+        deleteBtn.addEventListener("click", function (event) {
+            console.log("asd");
+            const carRow = document.getElementById("car-row-" + carId);
+            carRow.remove();
+        });
+
+    });
+}
 
 
 // Формирование формы для машин
-function createCarForm(carCounter){
+function createCarForm(carCounter) {
     const carForm = document.createElement('div');
     carForm.className = "car__row d-flex justify-content-between";
-    carForm.innerHTML ="<label for=\"number\" class=\"form-label text-center\">Номер машины\n" +
+    carForm.id = "car-row-" + carCounter;
+    carForm.innerHTML = "<label for=\"number\" class=\"form-label text-center\">Номер машины\n" +
         "                        <input type=\"text\"\n" +
-        "                               name=\"cars[car"+carCounter+"][number]\"\n" +
+        "                               name=\"cars[car" + carCounter + "][number]\"\n" +
         "                               id=\"number\"\n" +
         "                               class=\"form-control\"></label>\n" +
         "\n" +
         "                    <label for=\"driver_name\" class=\"form-label text-center\">Имя водителя\n" +
         "                        <input type=\"text\"\n" +
-        "                               name=\"cars[car"+carCounter+"][driver_name]\"\n" +
+        "                               name=\"cars[car" + carCounter + "][driver_name]\"\n" +
         "                               id=\"driver_name\"\n" +
-        "                               class=\"form-control\"> </label>";
+        "                               class=\"form-control\"> </label>" +
+        "                   <div class='delete_car' id=\"delete-" + carCounter + "\">Х</div>"
+    ;
     return carForm;
 }
